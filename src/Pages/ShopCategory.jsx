c135e6983a266969767d608a7c6c8b7945ba8f58
dilushamadushan <dmdmadushan007@gmail.com>
2025-08-16 19:05:19 +0530

    added item category for cloths

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { ProductContext } from '../Context/ProductContext'; 
 import NavBar from '../Components/NavBar';
-import BodyContent from '../Components/BodyContent';
+import all_product from '../assets/all_product';
+import Item from '../Components/Item';
 
 function ShopCategory(props) {
+    const {all_product} = useContext(ProductContext);
   return (
     <>
         <NavBar />
-        <BodyContent>
-            <h1 className="text-3xl font-bold">Welcome to Our {props.category} Category</h1>
-        </BodyContent>
+        <div className="flex flex-wrap justify-center gap-6 p-6">
+            {all_product.map((product,i) => {
+                if(props.category === product.category){
+                    return <Item key={i} id={product.id} name={product.name} img={product.image} new_price = {product.new_price} old_price = {product.old_price} />
+                }
+                else{
+                    return null;
+                }
+            })}
+        </div>
     </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ProductContext from './Context/ProductContext'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ProductContext>
     <App />
-  </StrictMode>,
+  </ProductContext>
 )
