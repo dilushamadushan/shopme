2dedbb6baf80ea84f242951cd3e24c15f3969fae
dilushamadushan <dmdmadushan007@gmail.com>
2025-08-30 23:16:34 +0530

    feat: refactor ProductContext to fetch products from API and initialize cart

--- fontend/src/Context/ProductContext.jsx
+++ fontend/src/Context/ProductContext.jsx
@@ -1,55 +1,70 @@
-import React,{createContext, useState} from "react";
-import all_product from "../assets/all_product";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ProductContext = createContext(null);
 
-const getDefaultCart = () => {
+  const getDefaultCart = () => {
     let cart = {};
-    for(let index = 0; index < all_product.length; index++){
-        cart[all_product[index].id] = 0;
+    for (let index = 0; index < 300; index++) {
+      cart[index] = 0;
     }
     return cart;
-}
+  };
 
-const ProductProvider = ({ children }) =>{
-    const [products, setProducts] = useState(all_product); 
-    const [cartItems, setCartItems] = useState(getDefaultCart())
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const addToCart = (itemId) =>{
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        console.log("Added to cart:", itemId);
+  useEffect(() => {
+    fetch("http://localhost:3000/allproduct")
+    .then((res) => res.json())
+    .then((data) => setProducts(data))
+  },[])
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log("Added to cart:", itemId);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = products.find((product) => product.id === Number(item));
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
     }
-    
-    const removeFromCart = (itemId) =>{
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-    }
+    return totalAmount;
+  };
 
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for(const item in cartItems){
-            if(cartItems[item] > 0){
-                let itemInfo = products.find((product) => product.id === Number(item));
-                totalAmount += itemInfo.new_price * cartItems[item];
-            }
-        } 
-        return totalAmount;
+  const getTotalCartItems = () => {
+    let totalItems = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItems += cartItems[item];
+      }
     }
+    return totalItems;
+  };
 
-    const getTotalCartItems = () => {
-        let totalItems = 0;
-        for(const item in cartItems){
-            if(cartItems[item] > 0){
-            totalItems += cartItems[item];
-            }
-        }
-        return totalItems;
-    }
-
-    return (
-        <ProductContext.Provider value={{ products, setProducts, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems }}>
-            {children}
-        </ProductContext.Provider>
-    )
-}
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        setProducts,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount,
+        getTotalCartItems,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
 
 export default ProductProvider;
