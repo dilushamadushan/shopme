b1e5dc7397191f71a8e9cc98d568211292e5874c
dilushamadushan <dmdmadushan007@gmail.com>
2025-08-31 17:58:51 +0530

    implement  user authentication

--- backend/index.js
+++ backend/index.js
@@ -123,7 +123,11 @@ const Users = mongoose.model('Users', {
   date : {
     type: Date,
     default : Date.now,
-  }
+  },
+  cartData: { 
+    type: Object,
+     default: {} 
+    },
 })
 
 app.post('/auth', async (req, res) => {
@@ -175,6 +179,42 @@ app.post('/login', async (req,res) => {
   }
 })
 
+const fetchUser = async (req, res, next) => {
+  const token = req.header('auth-token');
+  if(!token){
+    res.status(401).send({errors : "Please authenticate using valid token"})
+  }else{
+    try{
+      const data = jwt.verify(token,'secret_ecom');
+      req.user = data.user;
+      next();
+    }catch (error){
+      res.status(401).send({errors : "Please authanticate using valid token"})
+    }
+  }
+}
+
+app.post('/cart', fetchUser, async (req,res) => {
+  let userData = await Users.findOne({_id: req.user.id});
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+  res.send({success: true,message: "Added"})
+})
+
+app.post('/removeCart',fetchUser,async (req, res) => {
+  let userData = await Users.findOne({_id: req.user.id});
+  if(userData.cartData[req.body.itemId]  > 0)
+  userData.cartData[req.body.itemId] -= 1;
+  await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+  res.send({success: true,message:"Removed"})
+})
+
+app.post('/getcart', fetchUser, async (req, res) => {
+  console.log("getCart");
+  let userData = await Users.findOne({_id:req.user.id});
+  res.json(userData.cartData)
+  
+})
 
 mongoose.connect(`${process.env.MONGO_URL}`);
 
