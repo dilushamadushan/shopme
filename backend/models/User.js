f8e070faa1f08250cfa3f5e34271c8e5cf071a02
dilushamadushan <dmdmadushan007@gmail.com>
2025-09-05 22:16:51 +0530

    feat: add Product and User models with schemas for MongoDB

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  date: { type: Date, default: Date.now },
+  cartData: { type: Object, default: {} },
+});
+
+module.exports = mongoose.model("User", UserSchema);
