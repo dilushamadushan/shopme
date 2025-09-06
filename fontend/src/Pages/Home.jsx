f5fe60afa06c8c7b5809957cd1fc724ef95c0213
dilushamadushan <dmdmadushan007@gmail.com>
2025-09-06 21:51:10 +0530

    feat: remove BodyContent component and add Footer component to layout

--- fontend/src/Pages/Home.jsx
+++ fontend/src/Pages/Home.jsx
@@ -1,11 +1,8 @@
-import BodyContent from '../Components/BodyContent'
 
 function Home() {
   return (
     <>
-        <BodyContent>
-            <h1 className="text-3xl font-bold">Welcome to Our Shop</h1>
-        </BodyContent>
+      <h1 className="text-3xl font-bold">Welcome to Our Shop</h1>
     </>
   )
 }
--- fontend/src/layout/RootLayout.jsx
+++ fontend/src/layout/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 export default function RootLayout() {
     return(
         <>
             <NavBar/>
             <Outlet />
+            <Footer />
         </>
     )
 }
